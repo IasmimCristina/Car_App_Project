4757b1e1dd58fcabe507ad4733955f77c56f5836
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-10-06 16:07:40 -0300

    Initiating the project: global and tailwind added.

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className='hero__title'>Hello, im Ias!.</h1>
+      </div>
+      </div>
+  )
+}
+
+export default Hero
---
+++ components/index.ts
@@ -0,0 +1,7 @@
+// Here all the components will be exported
+
+import Hero from "./Hero";
+
+export {
+  Hero
+}
