33d6001cb3d7086c817e57c6ea482d2b4b7bbba3
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-10-20 17:05:58 -0300

    added translation

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -15,17 +15,126 @@ interface CarDetailesProps {
 }
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailesProps) => {
+
+
+  // Translation array
+  const keyTranslations = {
+    city_mpg: 'Cidade (MPG)',
+    class: 'Classe',
+    combination_mpg: 'Combinação (MPG)',
+    cylinders: 'Cilindros',
+    displacement: 'Deslocamento',
+    drive: 'Tração',
+    fuel_type: 'Tipo de Combustível',
+    highway_mpg: 'Rodovia (MPG)',
+    make: 'Fabricante',
+    model: 'Modelo',
+    transmission: 'Transmissão',
+    year: 'Ano',
+  };
+
   return (
 
-   
+
     <>
 
       <Transition appear show={isOpen} as={Fragment}>
 
         <Dialog as='div' className={"relative z-10"} onClose={closeModal}>
-          <Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+
+          >
             <div className='fixed inset-0 bg-black bg-opacity-25' />
-          </Transition.Child>    
+          </Transition.Child>
+
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+
+
+
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+
+              >
+
+
+                <Dialog.Panel
+                  className={"relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5"}
+                >
+                  <button
+                    type='button'
+                    onClick={closeModal}
+                    className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
+                  >
+
+                    <Image src={"/close.svg"} alt='close' width={20} height={20} className='object-contain' />
+
+                  </button>
+
+
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg ">
+
+                      <Image src="/hero.png" alt='Modelo de carro' fill priority className='object-contain' />
+
+
+                    </div>
+
+
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 bg-orange-50 rounded-lg">
+                        <Image src="/hero.png" alt='Modelo de carro' fill priority className='object-contain' />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-orange-50 rounded-lg">
+                        <Image src="/hero.png" alt='Modelo de carro' fill priority className='object-contain' />
+                      </div>
+                      <div className="flex-1 relative w-full h-24 bg-orange-50 rounded-lg">
+                        <Image src="/hero.png" alt='Modelo de carro' fill priority className='object-contain' />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex-1 flex gap-2 flex-col">
+                    <h2 className='font-semibold text-xl capitalize'>
+                      {car.make}
+                      {car.model}
+                    </h2>
+
+                    <div className="mt-3 flex flex-wrap gap-4">
+
+                      {/* Getting the key as the title for the dialog */}
+                      {Object.entries(car).map(([key, value]) => (
+                        <div className="flex justify-between gap-5 w-full text-right" key={key}>
+                          <h4 className='text-grey capitalize'>
+                            {keyTranslations[key] || key}
+                          </h4>
+                          <p className='text-black-100 font-semibold'>{value}</p>
+                        </div>
+                      ))}
+
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+
+
+
+            </div>
+          </div>
         </Dialog>
       </Transition>
 
