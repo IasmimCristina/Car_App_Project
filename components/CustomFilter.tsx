e7e25c4cdb8bad85d585d43cf79ab57c711b1bd4
IasmimCristina <iaasmimcristinaa@gmail.com>
2023-10-23 23:26:08 -0300

    problems with set manufacturer

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,19 +1,36 @@
 "use client"
+import React from 'react';
+import { Listbox } from '@headlessui/react';
+
 import { useState, Fragment } from 'react';
 import { useRouter } from 'next/navigation';
-import { Listbox, Transition } from '@headlessui/react';
+import { Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
 import Image from 'next/image';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
 
+
+  const handleUpdateParams = (e: { type: string, value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+
+
+    router.push(newPathName, { scroll: false })
+  }
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
-        onChange={(e) => setSelected(e)}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams({ type: "seuTipoAqui", value: e.value });
+        }}
       >
+
         <div className="relative w-fit z-10">
           <Listbox.Button className={"custom-filter__btn"}>
             <span className='block truncate'>{selected.title}</span>
@@ -42,7 +59,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
           </Transition>
         </div>
       </Listbox>
-    </div>
+    </div >
   );
 };
 
